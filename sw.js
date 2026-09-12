511ea9b8b4dfa580d2ccc47e3d6eba670ae814d2
k7kjr6jpv5-ops <k7kjr6jpv5@privaterelay.appleid.com>
2026-09-12 11:31:07 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-recipe-pwa-v3';
+const CACHE='my-recipe-pwa-v4';
 const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
